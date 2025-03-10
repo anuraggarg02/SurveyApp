fc8c32f3af7ec58050a6167a333a2c4a482c0c22
Anurag Garg <anuraggarg@Anurags-MacBook-Air.local>
2025-03-11 00:31:04 +0530

    added auth code

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./sevices/passport');
+const authRoutes = require('./routes/authRoutes');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+authRoutes(app);
 
 const PORT = process.env.PORT || 9000;
 
